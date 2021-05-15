41fd969dccd48b5a2ea87bef72e89afc5982873f
Sébastien Gahat <sgahat@octo.com>
2021-05-15 20:33:45 +0200

    affichage des deux panels + graphic primitives dans un array

--- src/paulgahat/App.java
+++ src/paulgahat/App.java
@@ -1,55 +1,38 @@
 package paulgahat;
 
-import java.awt.*;        // Using AWT container and component classes
-import java.awt.event.*;  // Using AWT event classes and listener interfaces
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
 
-// An AWT program inherits from the top-level container java.awt.Frame
 public class App extends Frame {
-   private Label lblCount;    // Declare a Label component
-   private TextField tfCount; // Declare a TextField component
-   private Button btnCount;   // Declare a Button component
-   private int count = 0;     // Counter's value
+    private String shapes[] = new String[] {
+            "outlined rectangle", "filled rectangle", "outlined oval" };
 
-   // Constructor to setup GUI components and event handlers
-   public App () {
-      Panel p1 = new Panel();
-      p1.add(new Label("Bonjour"));
+    public App () {
+        setTitle("Drawing Interface Paul Gahat");
+        setLayout(new BorderLayout());
 
-      p1.setLayout(new GridLayout(10,1));
-      p1.setSize(900,1000);
-      add(p1);
-      
-      Panel p2 = new Panel();
-      Choice cb = new Choice();
-	  	cb.add("outlined rectangle");
-		cb.add("filled rectangle");
-		cb.add("outlined oval");
-	  p2.add(cb);
-	  
-	  p2.setLayout(new BorderLayout());
-	  p2.setSize(100,1000);
-	  add(p2);                                  
+        Panel p1 = new Panel();
+        p1.setPreferredSize(new Dimension(500, 500));
+        p1.setLayout(new BorderLayout());
 
-//      BtnCountListener listener = new BtnCountListener();
-//      btnCount.addActionListener(listener);
+        Panel p2 = new Panel();
+        p2.setLayout(new FlowLayout());
+        Choice cb = new Choice();
+        for (int i = 0; i < shapes.length; i++) {
+            cb.add(shapes[i]);
+        }
+        p2.add(cb);
 
-	  setLayout(new GridLayout(2,1));
-      setTitle("Drawing Interface Paul Gahat");  
-      setSize(1000, 1000);        
-      setVisible(true);
-   }
+        add("North", p1);
+        add("South", p2);
 
-   public static void main(String[] args) {
-      App app = new App();
-   }
+        pack();
+        setVisible(true);
+    }
 
-   // Define an inner class to handle the "Count" button-click
-   private class BtnCountListener implements ActionListener {
-      // ActionEvent handler - Called back upon button-click.
-      @Override
-      public void actionPerformed(ActionEvent evt) {
-         ++count;
-         tfCount.setText(count + "");
-      }
-   }
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println("Allowed Graphic Primitives : " + Arrays.toString(app.shapes));
+    }
 }
